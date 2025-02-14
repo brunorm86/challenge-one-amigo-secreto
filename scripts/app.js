15824cc2e899e64dc294a99739100ee6b948eca3
Bruno Ricardo Machado <bruno.rm@grad.ufsc.br>
2025-02-14 13:03:26 -0300

    validações ao incluir amigos concluídas

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+var listaAmigos = [];
+
+function verificarSeAmigoRepetido(amigo){
+return listaAmigos.includes(amigo)
+}
+
+function verificarSeAmigoInvalido(amigo){
+    if(amigo=="" || amigo==null || !isNaN(amigo)){
+        return true;
+}
+}
+
+function adicionarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+
+    amigo = amigo.trim();
+
+    if(verificarSeAmigoInvalido(amigo)){
+        alert("O nome do amigo não pode ser vazio ou apenas números!");
+    } else if (verificarSeAmigoRepetido(amigo)){
+        alert("O nome "+amigo+" já existe na lista!");
+    } else{
+    listaAmigos.push(amigo)
+    }
+    console.log(listaAmigos)
+    document.getElementById("amigo").value = "";
+    }
+
+function sortearAmigo(){
+
+    if (listaAmigos.length==0){
+        alert("A lista de amigos está vazia!")
+    } else {
+
+    return amigo;
+}
+}
